b19038760bbdb1bf1e94dd2d0ece0c9c5d80ad4b
Shoaib <Shoaib@Shoaib-Surface>
2017-03-27 14:29:49 +0100

    type issue when sending boolean, boolean, String to boolean, boolean, String must resolve by checking JRE

---
+++ LogicGateSim/src/AI.java
@@ -0,0 +1,145 @@
+
+public class AI {
+	
+	public Boolean logicGates[][] = {
+	
+	//						Input A		Input B
+	//XOR true Statements
+							{false,		true},
+							{true,		false},
+	//OR True Statements
+							{false,		true},
+							{true,		false},
+							{true,		true},
+	//NOR true Statement
+							{false,		false},
+	//AND true Statement
+							{true,		true},
+	//NAND true Statements
+							{false,		false},
+							{false,		true},
+							{true,		false}
+	};
+	
+	boolean inputA;
+	boolean inputB;
+	boolean output;
+	String Gate = "";
+	
+
+
+	public AI(boolean getinputA, boolean getinputB, String getGate) {
+		gateOutput(getinputA, getinputB, getGate);
+	}
+	
+	public boolean gateOutput( boolean getinputA, boolean getinputB, String getGate) {
+		inputA = getinputA;
+		inputB = getinputB;
+		Gate = getGate;
+		
+		int row;
+		int column = 0;
+		
+		//output set to false less otherwise
+		boolean output = false;
+		
+		if(Gate == "XOR"){
+			for(row = 0; row < 2; row = row++){
+				if ((logicGates[row][column] == inputA) && (logicGates[row][column + 1] == inputB)){
+					output = true;
+				}
+			}
+		}
+		
+		else if (Gate == "OR"){
+			for(row = 2; row < 5; row = row++){
+				if ((logicGates[row][column] == inputA) && (logicGates[row][column + 1] == inputB)){
+					output = true;
+				}
+			}
+		}
+		
+		else if (Gate == "NOR"){
+			row = 5;
+			if ((logicGates[row][column] == inputA) && (logicGates[row][column + 1] == inputB)){
+				output = true;
+			}
+		}
+		
+		
+		else if (Gate == "AND"){
+			row = 6;
+			if ((logicGates[row][column] == inputA) && (logicGates[row][column + 1] == inputB)){
+				output = true;
+			}
+		}
+		
+		else if (Gate == "NAND"){
+			for(row = 7; row < 10; row = row++){
+				if ((logicGates[row][column] == inputA) && (logicGates[row][column + 1] == inputB)){
+					output = true;
+				}
+			}
+		}
+		return output;
+	}
+	
+	public boolean main(String[] args, boolean getinputA, boolean getinputB, String getGate) {
+		AI ai = new AI(getinputA, getinputB, getGate); //to redo
+		return output = gateOutput(getinputA, getinputB, getGate);
+		
+		//store inputs locally
+		/*inputA = getinputA;
+		inputB = getinputB;
+		Gate = getGate;
+		
+		int row;
+		int column = 0;
+		
+		//output set to false less otherwise
+		boolean output = false;
+		
+		if(Gate == "XOR"){
+			for(row = 0; row < 2; row = row++){
+				if (logicGates[row][column] == inputA && logicGates[row][column + 1] == inputB){
+					output = true;
+					row = 2;
+				}
+			}
+		}
+		
+		if (Gate == "OR"){
+			for(row = 2; row < 5; row = row++){
+				if (logicGates[row][column] == inputA && logicGates[row][column + 1] == inputB){
+					output = true;
+					row = 5;
+				}
+			}
+		}
+		
+		if (Gate == "NOR"){
+			row = 5;
+			if (logicGates[row][column] == inputA && logicGates[row][column + 1] == inputB){
+				output = true;
+			}
+		}
+		
+		
+		if (Gate == "AND"){
+			row = 6;
+			if (logicGates[row][column] == inputA && logicGates[row][column + 1] == inputB){
+				output = true;
+			}
+		}
+		
+		if (Gate == "NAND"){
+			for(row = 7; row < 10; row = row++){
+				if (logicGates[row][column] == inputA && logicGates[row][column + 1] == inputB){
+					output = true;
+					row = 10;
+				}
+			}
+		}*/
+	}		
+}
+	
--- LogicGateSim/src/drawing.java
+++ LogicGateSim/src/drawing.java
@@ -3,7 +3,7 @@
  * Author: 			Shoaib Waseem
  * Student Code:	w13013878
  * University:		Northumbria University
- * Version:			Version 2
+ * Version:			Version4
  * Date:			20th March 2017 
  * 
  * 
@@ -12,10 +12,9 @@
  * 
  */
 
-
-	/***************************************************************************************/
-	/**************************************IMPORT MODULES***********************************/
-	/***************************************************************************************/
+/***************************************************************************************/
+/**************************************IMPORT MODULES***********************************/
+/***************************************************************************************/
 import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
@@ -23,285 +22,412 @@ import java.util.Random;
 
 import javax.swing.*;
 
-
-public class drawing extends JFrame implements ActionListener
-{
+public class drawing extends JFrame implements ActionListener {
 	/***************************************************************************************/
-	/**************************************DEFINITIONS**************************************/
+	/************************************** DEFINITIONS **************************************/
 	/***************************************************************************************/
-	JPanel palette,tools;
-    JButton orGate, andGate, xorGate, norGate, nandGate, confirmNumberOfGates, randomTest;
-    
+	JPanel palette, tools, paletteTT;
+	JButton orGate, andGate, xorGate, norGate, nandGate, confirmNumberOfGates,
+			randomTest, checkAnswers;
+	JTable truthTable;
+	JScrollPane scrollPane;
 
-    MyCanvas drawarea;
-    
-    Color background = new Color(128,128,127);
-    
-    
-    
-    //Drawing Classes through buttons
-    
-    drawingOR ORGate; 		// OR gate class
-    drawingAND ANDGate; 	// AND gate class
-    drawingXOR XORGate; 	// XOR gate class
-    drawingNOR NORGate;		// NOR gate class
-    drawingNAND NANDGate;	// NAND gate class
-    JComboBox cbx_numberOfGates;
-    
-    //Canvas Size
-    int canvasSizeX = 800;
-    int canvasSizeY = 800;
-    
-    // Coordinate integers
-    int gate; // 1=OR, 2=NAND, 3=XOR, 4=NOR, 5=NAND
-    
-    //coordinate translation for multiple gates
+	MyCanvas drawarea;
+
+	Color background = new Color(128, 128, 127);
+
+	// Drawing Classes through buttons
+
+	drawingOR ORGate; // OR gate class
+	drawingAND ANDGate; // AND gate class
+	drawingXOR XORGate; // XOR gate class
+	drawingNOR NORGate; // NOR gate class
+	drawingNAND NANDGate; // NAND gate class
+	JComboBox cbx_numberOfGates;
+
+	// Canvas Size
+	int canvasSizeX = 1200;
+	int canvasSizeY = 800;
+
+	// Coordinate integers
+	int gate; // 1=OR, 2=NAND, 3=XOR, 4=NOR, 5=NAND
+
+	// coordinate translation for multiple gates
 	int translationX;
 	int translationY;
-	
+
 	int cbx_index = 0;
 	int index = 0;
-	
-	Color currentColour = new Color(0,0,0); // black line for drawing
-	
-	//used for the combo box, 
-	String[] numberOfGates  = { "Select No. of Gates","1","3","7" };
-	
+
+	Color currentColour = new Color(0, 0, 0); // black line for drawing
+
+	// used for the combo box,
+	String[] numberOfGates = { "Select No. of Gates", "1", "3" };
+
 	int[] threeGatesList = new int[3];
-	
-	//co-ordinates used for 1 gate combo box selection - x, y
-	int[] oneGateCoord = {300, 400};
-	
-	//rng for random gate
+
+	// co-ordinates used for 1 gate combo box selection - x, y
+	int[] oneGateCoord = { 300, 400 };
+
+	// rng for random gate
 	Random randomGate = new Random();
-    
-	//List of printable Gates
+
+	// AI
+	AI AI;
+
+	// List of printable Gates
 	ArrayList<Gates> GatesList = new ArrayList<Gates>();
 	Gates XOR = new Gates(1);
 	Gates OR = new Gates(2);
 	Gates NOR = new Gates(3);
 	Gates AND = new Gates(4);
 	Gates NAND = new Gates(5);
-	
-	//List of Printed Gates
-	ArrayList<Gates> printedGates = new ArrayList<Gates>();
-	
 
-    public drawing()
-    {
-	setSize(canvasSizeX,canvasSizeY); 
-	palette = new JPanel(); // Watch where this gets placed to
-	palette.setLayout(new GridLayout(2,3,5,5)); // grid layout
-     
-    setTitle("Logic Gates"); 
-    
-    //add gates to arraylist GatesList
-    if(GatesList.isEmpty()){
-        GatesList.add(XOR);
-        GatesList.add(OR);
-        GatesList.add(NOR);
-        GatesList.add(AND);
-        GatesList.add(NAND);
-    }
+	// List of Printed Gates
+	ArrayList<String> printedGates = new ArrayList<String>();
 
+	// Column Names
+	// test save
+	String[] ColumnHeadings = { "A", "B", "C", "D", "E", "F", "G" };
 
-    
-    // Instance of classes
-	ORGate = new drawingOR(); 			// drawingOR.java
-	ANDGate = new drawingAND(); 		// drawingAND.java
-	XORGate = new drawingXOR(); 		// drawingXOR.java
-	NORGate = new drawingNOR(); 		// drawingNOR.java
-	NANDGate = new drawingNAND(); 		// drawingNAND.java
-	
+	// 3 Gates Array
+	String[][] GatesArrayS = {
+			// A B C D E F G
+			{ "0", "0", "0", "0", "", "", "" },
+			{ "0", "0", "0", "1", "", "", "" },
+			{ "0", "0", "1", "0", "", "", "" },
+			{ "0", "0", "1", "1", "", "", "" },
+			{ "0", "1", "0", "0", "", "", "" },
+			{ "0", "1", "0", "1", "", "", "" },
+			{ "0", "1", "1", "0", "", "", "" },
+			{ "0", "1", "1", "1", "", "", "" },
+			{ "1", "0", "0", "0", "", "", "" },
+			{ "1", "0", "0", "1", "", "", "" },
+			{ "1", "0", "1", "0", "", "", "" },
+			{ "1", "0", "1", "1", "", "", "" },
+			{ "1", "1", "0", "0", "", "", "" },
+			{ "1", "1", "0", "1", "", "", "" },
+			{ "1", "1", "1", "0", "", "", "" },
+			{ "1", "1", "1", "1", "", "", "" }, };
 
-	
-	
-	// Assign components with variables
-	orGate = new JButton("OR Gate"); 
-	andGate = new JButton("AND Gate");
-	xorGate = new JButton("XOR Gate");
-	norGate = new JButton("NOR Gate");
-	nandGate = new JButton("NAND Gate");
-	randomTest = new JButton("Random");
-	confirmNumberOfGates = new JButton("Confirm");
-	
-	//used to determine how complex the logic gate system will be
-	cbx_numberOfGates = new JComboBox(numberOfGates);
-	palette.add(cbx_numberOfGates);
-	cbx_numberOfGates.setSelectedIndex(0);
-	cbx_numberOfGates.addActionListener(this);
-	
-	// Assign button to confirm number of logic gates
-	palette.add(confirmNumberOfGates);
-	confirmNumberOfGates.addActionListener(this);
-	
-	// Assign button to draw a single logic gate - for testing
-	palette.add(orGate);
-	orGate.addActionListener(this);
-	palette.add(andGate);
-	andGate.addActionListener(this);
-	palette.add(xorGate);
-	xorGate.addActionListener(this);
-	palette.add(norGate);
-	norGate.addActionListener(this);
-	palette.add(nandGate);
-	nandGate.addActionListener(this);
-	palette.add(randomTest);
-	randomTest.addActionListener(this);
-	
-	// Gate buttons on top
-	getContentPane().add(palette,"North"); 
+	Boolean[][] GatesArray = {
+			{ false, false, false, false, false, false, false },
+			{ false, false, false, true, false, false, false },
+			{ false, false, true, false, false, false, false },
+			{ false, false, true, true, false, false, false },
+			{ false, true, false, false, false, false, false },
+			{ false, true, false, true, false, false, false },
+			{ false, true, true, false, false, false, false },
+			{ false, true, true, true, false, false, false },
+			{ true, false, false, false, false, false, false },
+			{ true, false, false, true, false, false, false },
+			{ true, false, true, false, false, false, false },
+			{ true, false, true, true, false, false, false },
+			{ true, true, false, false, false, false, false },
+			{ true, true, false, true, false, false, false },
+			{ true, true, true, false, false, false, false },
+			{ true, true, true, true, false, false, false },
 
-	// Draw area
-	drawarea = new MyCanvas(); // MyCanvas class
-	getContentPane().add(drawarea,"Center"); // Draw stuff in the centre
-	setVisible(true); // visible true for all elements
-	} 
-	       
+	};
 
-    public void actionPerformed(ActionEvent ev)
-    {
-    	
-		if(("Confirm".equals(ev.getActionCommand())))
-		{
+	boolean output;
+
+	public drawing() {
+
+		setSize(canvasSizeX, canvasSizeY);
+		palette = new JPanel(); // Watch where this gets placed to
+		paletteTT = new JPanel();
+		palette.setLayout(new GridLayout(2, 3, 5, 5)); // grid layout
+		setTitle("Logic Gates");
+
+		// drawTruthTable();
+
+		// add gates to arraylist GatesList
+		if (GatesList.isEmpty()) {
+			GatesList.add(XOR);
+			GatesList.add(OR);
+			GatesList.add(NOR);
+			GatesList.add(AND);
+			GatesList.add(NAND);
+		}
+
+		// initialise AI
+
+		// Instance of classes
+		ORGate = new drawingOR(); // drawingOR.java
+		ANDGate = new drawingAND(); // drawingAND.java
+		XORGate = new drawingXOR(); // drawingXOR.java
+		NORGate = new drawingNOR(); // drawingNOR.java
+		NANDGate = new drawingNAND(); // drawingNAND.java
+
+		// Assign components with variables
+		orGate = new JButton("OR Gate");
+		andGate = new JButton("AND Gate");
+		xorGate = new JButton("XOR Gate");
+		norGate = new JButton("NOR Gate");
+		nandGate = new JButton("NAND Gate");
+		randomTest = new JButton("Random");
+		checkAnswers = new JButton("Check");
+		confirmNumberOfGates = new JButton("Confirm");
+
+		// used to determine how complex the logic gate system will be
+		cbx_numberOfGates = new JComboBox(numberOfGates);
+		palette.add(cbx_numberOfGates);
+		cbx_numberOfGates.setSelectedIndex(0);
+		cbx_numberOfGates.addActionListener(this);
+
+		// Assign button to confirm number of logic gates
+		palette.add(confirmNumberOfGates);
+		confirmNumberOfGates.addActionListener(this);
+
+		// Assign button to draw a single logic gate - for testing
+		palette.add(orGate);
+		orGate.addActionListener(this);
+		palette.add(andGate);
+		andGate.addActionListener(this);
+		palette.add(xorGate);
+		xorGate.addActionListener(this);
+		palette.add(norGate);
+		norGate.addActionListener(this);
+		palette.add(nandGate);
+		nandGate.addActionListener(this);
+		palette.add(randomTest);
+		randomTest.addActionListener(this);
+
+		// Gate buttons on top
+		getContentPane().add(palette, "North");
+
+		// Truth Tables
+
+		// truthTable = new JTable(GatesArray, ColumnHeadings);
+		truthTable = new JTable(GatesArray, ColumnHeadings);
+		// paletteTT.add(truthTable);
+		paletteTT.add(new JScrollPane(truthTable));
+		paletteTT.add(checkAnswers);
+		getContentPane().add(paletteTT, "South");
+
+		// Draw area
+		drawarea = new MyCanvas(); // MyCanvas class
+		getContentPane().add(drawarea, "Center"); // Draw stuff in the centre
+		setVisible(true); // visible true for all elements
+
+	}
+
+	public void actionPerformed(ActionEvent ev) {
+
+		if (("Confirm".equals(ev.getActionCommand()))) {
 			cbx_index = cbx_numberOfGates.getSelectedIndex();
 
-						
-			//Random Gate Generation	
-			if(cbx_index != 0){
-				//if gate number is chosen
+			// Random Gate Generation
+			if (cbx_index != 0) {
+				// if gate number is chosen
+				printedGates.clear();
 				repaint();
-			
+
 			} else {
-				{JOptionPane.showMessageDialog(null, "Please select a logic gate first");}
+				{
+					JOptionPane.showMessageDialog(null,
+							"Please select a logic gate first");
+				}
 			}
 
 		}
-				
-    }
-				
-    
-    
+	}
+
 	/***************************************************************************************/
-	/**************************************MAIN CLASS***************************************/
+	/************************************** MAIN CLASS ***************************************/
 	/***************************************************************************************/
-    public static void main(String args[])
-    {
+	public static void main(String args[]) {
 		new drawing(); // Calling main prog
-    }
-
-
+	}
 
 	/***************************************************************************************/
-	/**************************************CANVAS CLASS*************************************/
+	/************************************** CANVAS CLASS *************************************/
 	/**************************************************************************************/
-	class MyCanvas extends JPanel implements MouseListener,  MouseMotionListener
-	{
-		public MyCanvas()
-		{
-			addMouseListener(this); 
-			//addMouseMotionListener( this);
+	class MyCanvas extends JPanel implements MouseListener, MouseMotionListener {
+		public MyCanvas() {
+			addMouseListener(this);
+			// addMouseMotionListener( this);
 
 			setVisible(true); // Better to set visible than to not
 		}
-		
-		
-    
-		public void updateGates(Graphics gfx) 
-		{	
 
-			index = randomGate.nextInt(GatesList.size());
-			printedGates.add(GatesList.get(index));
+		public void updateGates(Graphics gfx) {
+
+			index = randomGate.nextInt((GatesList.size() + 1) - 1);
+			System.out.println(index);
 			GatesList.get(index).getGate(gfx, translationX, translationY);
+			addPrintedGate();
+			completeTable();
+
 		}
-		
+
+		public void addPrintedGate() {
+			if (index == 0) {
+				printedGates.add("XOR");
+			} else if (index == 1) {
+				printedGates.add("OR");
+			} else if (index == 2) {
+				printedGates.add("NOR");
+			} else if (index == 3) {
+				printedGates.add("AND");
+			} else if (index == 4) {
+				printedGates.add("NAND");
+			}
+		}
+
+		public void completeTable() {
+			int ttRows = 15;
+			boolean inputA;
+			boolean inputB;
+			for (int i = 0; i < 3; i++) {
+				if (i < 2) {
+					if (printedGates.get(i) == "XOR") {
+
+						for (int j = 0; j < ttRows; j++) {
+							inputA = GatesArray[0][j];
+							System.out.println("input A: " + inputA);
+							inputB = GatesArray[1][j];
+							System.out.println("input B: " + inputB);
+							output = AI.gateOutput(inputA, inputB, "NOR");
+						}
+
+					} else if (printedGates.get(i) == "OR") {
+						if (i == 0) {
+							for (int j = 0; j < ttRows; j++) {
+								inputA = GatesArray[0][j];
+								System.out.println("input A: " + inputA);
+								inputB = GatesArray[1][j];
+								System.out.println("input B: " + inputB);
+								output = AI.gateOutput(inputA, inputB, "NOR");
+							}
+						}
+
+					} else if (printedGates.get(i) == "NOR") {
+						for (int j = 0; j < ttRows; j++) {
+							inputA = GatesArray[0][j];
+							System.out.println("input A: " + inputA);
+							inputB = GatesArray[1][j];
+							System.out.println("input B: " + inputB);
+							output = AI.gateOutput(inputA, inputB, "NOR");
+							System.out.println(output);
+						}
+
+					} else if (printedGates.get(i) == "AND") {
+						for (int j = 0; j < ttRows; j++) {
+							inputA = GatesArray[0][j];
+							System.out.println("input A: " + inputA);
+							inputB = GatesArray[1][j];
+							System.out.println("input B: " + inputB);
+							output = AI.gateOutput(inputA, inputB, "NOR");
+						}
+
+					} else if (printedGates.get(i) == "NAND") {
+						for (int j = 0; j < ttRows; j++) {
+							inputA = GatesArray[0][j];
+							System.out.println("input A: " + inputA);
+							inputB = GatesArray[1][j];
+							System.out.println("input B: " + inputB);
+							output = AI.gateOutput(inputA, inputB, "NOR");
+						}
+
+					}
+				}
+			}
+		}
+
 		public void updateSingleGate(Graphics gfx) {
 			translationX = translationX + 270;
 			translationY = translationY + 75;
 			updateGates(gfx);
 		}
-			
-		
-		public void updateFinalGate(Graphics gfx){
-			
+
+		public void updateFinalGate(Graphics gfx) {
+
 			translationX = translationX + 150;
 			translationY = translationY - 150;
 			updateGates(gfx);
 		}
-		
+
 		public void updateTwoGates(Graphics gfx) {
-			
-			for (int i = 0; i < 2; i++)
-			{
+
+			for (int i = 0; i < 2; i++) {
 				updateGates(gfx);
+				
 				translationY = translationY + 100;
 			}
 		}
-		
-		public void updateFourGates(Graphics gfx){
-			
-			for (int i = 0; i < 4; i++)
-			{
+
+		public void updateFourGates(Graphics gfx) {
+
+			for (int i = 0; i < 4; i++) {
 				updateGates(gfx);
 				translationY = translationY + 100;
 			}
-		
+
 			translationX = translationX + 150;
 			translationY = translationY - (100 * 3);
 		}
-		
-		//for testing array list printedGates
-		public void getAll(){
-			for(Gates printed : printedGates) {
+
+		// for testing array list printedGates
+		public void getAll() {
+			for (String printed : printedGates) {
 				System.out.println(printed);
 			}
 		}
 
-		
-		
-		public void paint(Graphics gfx) 
-		{	
-		gfx.setColor(currentColour);
-		translationX = 0;
-		translationY = 0;
-		printedGates.clear();
-		
-		if(cbx_index == 1){
-			
-			updateSingleGate(gfx);
-		}
-		
-		if(cbx_index == 2) {
-			
-			updateTwoGates(gfx);
-			updateFinalGate(gfx);
-				
+		public void paint(Graphics gfx) {
+			gfx.setColor(currentColour);
+			translationX = 400;
+			translationY = 0;
+			printedGates.clear();
+
+			if (cbx_index == 1) {
+
+				updateSingleGate(gfx);
 			}
-		
-		if(cbx_index == 3) {
-			
-			updateFourGates(gfx);
-			updateTwoGates(gfx);
-			updateFinalGate(gfx);
-			
+
+			else if (cbx_index == 2) {
+
+				updateTwoGates(gfx);
+				updateFinalGate(gfx);
+
 			}
-		getAll();
+
+			else if (cbx_index == 3) {
+
+				updateFourGates(gfx);
+				updateTwoGates(gfx);
+				updateFinalGate(gfx);
+
+			}
+			getAll();
 		}
-		
-		
-		public void update(Graphics gfx)
-		{
+
+		public void update(Graphics gfx) {
 			super.paint(gfx);
 		}
-				
+
 		// All of this needs to be enabled
-		public void mouseReleased(MouseEvent e)	{}
-		public void mouseDragged(MouseEvent e)	{}
-		public void mousePressed(MouseEvent e)	{}
-		public void mouseClicked(MouseEvent e)	{}
-		public void mouseEntered(MouseEvent e)	{}
-		public void mouseExited(MouseEvent e)	{}
-		public void mouseMoved(MouseEvent e)	{}
+		public void mouseReleased(MouseEvent e) {
+		}
+
+		public void mouseDragged(MouseEvent e) {
+		}
+
+		public void mousePressed(MouseEvent e) {
+		}
+
+		public void mouseClicked(MouseEvent e) {
+		}
+
+		public void mouseEntered(MouseEvent e) {
+		}
+
+		public void mouseExited(MouseEvent e) {
+		}
+
+		public void mouseMoved(MouseEvent e) {
+		}
 	}
 }
-	
